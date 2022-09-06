ec52c8a26321d778b27f63169001740d453c9e8f
Max Alejandro <max.flores.ar@gmail.com>
2022-09-06 13:13:42 -0500

    UNA COMA

--- src/web/app.jsx
+++ src/web/app.jsx
@@ -88,7 +88,7 @@ const App = () => {
                 <div class="explicacion">
                     <p>Básicamente ingresas un numero "X" si este es par se divide entre 2, si es impar se multiplica por 3 y se le suma 1</p>
                     <h2>Utilidad</h2>
-                    <p>Ninguna esta cosa no sirve para nada, literalmente todo matemático te dirá que es un perdida de tiempo y lo es pero quería hacer un programa que lo graficara. Fin.</p>
+                    <p>Ninguna, esta cosa no sirve para nada, literalmente todo matemático te dirá que es un perdida de tiempo y lo es pero quería hacer un programa que lo graficara. Fin.</p>
                     <p>PD: Creo que por ahí hay un fallo con algunos números pares pero bueno ni modo</p>
                 </div>
             </div>
